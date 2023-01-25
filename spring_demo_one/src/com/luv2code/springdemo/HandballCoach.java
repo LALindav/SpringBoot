b80329f996980ae72669818a07b2bdc2739bac7d
LALindav <114574183+LALindav@users.noreply.github.com>
2023-01-25 14:11:03 +0100

    Copie du projet

---
+++ spring_demo_one/src/com/luv2code/springdemo/HandballCoach.java
@@ -0,0 +1,15 @@
+package com.luv2code.springdemo;
+
+public class HandballCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		return "Catch the ball with your hand";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
